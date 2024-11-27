6000f74a8d29fd1e8059086d09537a77b685c67b
Manuel Lopez <mp.arturolopez@gmail.com>
2024-11-27 17:44:48 -0500

    using preact

---
+++ web/src/components/containsDuplicateComponent.tsx
@@ -0,0 +1,128 @@
+import { useState } from 'preact/hooks';
+
+function ListItem({ listItem }: { listItem: string | string[] }) {
+  return (
+    <li>
+      {Array.isArray(listItem) ? <List list={listItem} /> : <p>{listItem}</p>}
+    </li>
+  );
+}
+
+function List({ list }: { list: (string | string[])[] }) {
+  return (
+    <ul>
+      {list.map((listItem) => (
+        <ListItem listItem={listItem} />
+      ))}
+    </ul>
+  );
+}
+export default function ContainsDuplice({
+  title,
+  description,
+  constraints,
+  ideas,
+  tests,
+  code,
+}: {
+  title: string;
+  description: string;
+  constraints: string[];
+  ideas: {
+    title: string;
+    timeComplexity: string;
+    spaceComplexity: string;
+    steps: (string | string[])[];
+  }[];
+  tests: string[];
+  code: string;
+}) {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  return (
+    <div
+      style={{
+        width: '100%',
+      }}
+    >
+      <h1>{title}</h1>
+      <div>
+        <ul
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            width: '20%',
+            listStyle: 'none',
+            padding: 0,
+            justifyContent: 'space-between',
+          }}
+        >
+          <li
+            onClick={() => {
+              setCurrentTab(0);
+            }}
+          >
+            Description
+          </li>
+          <li
+            onClick={() => {
+              setCurrentTab(1);
+            }}
+          >
+            Canvas
+          </li>
+          <li
+            onClick={() => {
+              setCurrentTab(2);
+            }}
+          >
+            Tests
+          </li>
+        </ul>
+      </div>
+      <div
+        id='parent'
+        style={{ display: 'flex', flexDirection: 'row', width: '100%' }}
+      >
+        <div id='left' style={{ width: '40%' }}>
+          {currentTab === 0 && (
+            <>
+              <p>{description}</p>
+            </>
+          )}
+          {currentTab === 1 && (
+            <>
+              <div>
+                <h2>Constraints</h2>
+                <ul>
+                  {constraints.map((c) => {
+                    return <li>{c}</li>;
+                  })}
+                </ul>
+              </div>
+              <div>
+                <h2>Ideas</h2>
+                {ideas.map((i) => {
+                  return (
+                    <div>
+                      <p>
+                        {i.title} Time: {i.timeComplexity} Space:{' '}
+                        {i.spaceComplexity}
+                        <List list={i.steps} />
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+            </>
+          )}
+          {currentTab === 2 && <></>}
+        </div>
+        <div id='code' style={{ width: '60%' }}>
+          <h2>Code</h2>
+          <div>{code}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
